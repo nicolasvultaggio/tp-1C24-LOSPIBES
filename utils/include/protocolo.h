97d9d949185b713719490238095fbdaa04dd4dc6
tomasRodriguezSPuch <trodriguezspuch@frba.utn.edu.ar>
2024-04-13 22:09:40 +0000

    Declaramos todas las funciones de protocolo en el .h

--- utils/include/protocolo.h
+++ utils/include/protocolo.h
@@ -11,6 +11,18 @@
 #include<assert.h>
 #include <commons/collections/queue.h>
 
+void crear_buffer(t_paquete* paquete);
+void* serializar_paquete(t_paquete* paquete, int bytes);
+void enviar_mensaje(char* mensaje, int socket_cliente);
+t_paquete* crear_paquete(void);
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
+void enviar_paquete(t_paquete* paquete, int socket_cliente);
+void eliminar_paquete(t_paquete* paquete);
+t_list* recibir_paquete(int socket_cliente);
+void* recibir_buffer(int* size, int socket_cliente);
+int recibir_operacion(int socket_cliente);
+void recibir_mensaje(int socket_cliente);
+
 typedef struct
 {
 	op_code codigo_operacion;
