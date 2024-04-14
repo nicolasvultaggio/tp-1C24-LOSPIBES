7c896ddcb74a599050eda61f858130563ad24330
Marcelocabezasflores <mcabezasflores@frba.utn.frba.edu.ar>
2024-04-14 19:52:53 +0000

    conexion CPU

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,8 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/include/hello.h>
+#include </home/utnso/Desktop/TRABAJOPRACTICOSO/tp-2024-1c-Grupo-5/cpu/include/main.h>
 
 int main(int argc, char* argv[]) {
     decir_hola("CPU");
     return 0;
+
+     t_log* logger= log_create("cpu.log","CPU",true, LOG_LEVEL_INFO);
+      int canalmemoria=0;
+      if (!crear_conexion(logger,&canalmemoria)){
+    liberar_conexion(logger);
+       }
+    return EXIT_FAILURE;
+    int x;
+    scanf("&d",&x);
+    enviar_mensaje("SOY CPU PUDE CONECTAR", canalmemoria);
+      liberar crear_conexion(logger);
+      return EXIT_SUCCESS;
+
 }
