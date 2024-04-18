8c90791aecf4ac42bbf15c6656cb8f19d77527f8
Marcelocabezasflores <mcabezasflores@frba.utn.edu.ar>
2024-04-18 05:05:27 +0000

    Conexion CPU(cliente) a MEMORIA(servidor)

--- cpu/include/main.h
+++ cpu/include/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <../../utils/include/hello.h>
@@ -7,4 +8,25 @@
 #include <../../utils/include/protocolo.h>
 #include <commons/config.h>
 #include <commons/log.h>
+int server_socket;
+int socket_cliente_a_CPU;
+/*int socket_cliente_a_MEMORIA;*/
+
+char* mensaje1;
+char* ip__propio;
+char* puerto__propio; 
+char* ip__memoria;
+char* puerto__memoria;
+
+int* handshakeDeCPU;
+(*handshakeDeCPU) = 0;
+
+
+
+t_log* logger;
+t_config* config1;
+
+bool iniciar_conexiones();
+void terminar_programa();
+void leer__configuraciones();
 #endif
--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,22 +1,58 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <utils/include/hello.h>
-#include </home/utnso/Desktop/TRABAJOPRACTICOSO/tp-2024-1c-Grupo-5/cpu/include/main.h>
+#include <../../cpu/include/main.h>
 
 int main(int argc, char* argv[]) {
     decir_hola("CPU");
+    //CREO CONEXION CPU A MEMORIA Y RECIBO MENSAJE//
+
+    logger = log_create("cpu_logs.log","cpu",1,LOG_LEVEL_INFO);
+    config1 = config_create("./configs/cpu.config");
+
+    leer__configuraciones();
+    int fdcpu;//filedescriptorCPU
+    fdcpu= crear_conexion(ip__memoria,puerto__memoria);
+
+    if(!fdcpu){
+        log_error(logger,"Alguna conexion esta tirando error");
+        terminar_programa();
+        exit(2);
+    }
+
+    enviar_operacion(fdcpu, &handshakeDeCPU);
+
+    char mensaje1[1024] = {0};
+    recv(fdcpu, mensaje1, 1024, 0);
+    printf("Respuesta del servidor: %s\n", mensaje1);    
+
+    close(fdcpu);
+
+    
+
+    /*server_socket = iniciar_servidor(NULL ,puerto__propio);
+    int cliente_socket = esperar_cliente(server_socket);*/
+    
+    terminar_programa();
     return 0;
+  
 
-     t_log* logger= log_create("cpu.log","CPU",true, LOG_LEVEL_INFO);
-      int socket_server_MEMORIA=0;
-      if (!crear_conexion(logger,&socket_server_MEMORIA)){
-    liberar_conexion(logger);
-       }
-    return EXIT_FAILURE;
-    int x;
-    scanf("&d",&x);
-    enviar_mensaje("SOY CPU PUDE CONECTAR", socket_server_MEMORIA);
-      liberar crear_conexion(logger);
-      return EXIT_SUCCESS;
+}
 
+void leer__configuraciones(){
+    ip__propio= config_get_string_value(config1,"IP__ESCUCHA");
+    puerto__propio = config_get_string_value(config1,"PUERTO__ESCUCHA");
+    ip__memoria = config_get_string_value(config1,"IP__MEMORIA");
+    puerto__memoria = config_get_string_value(config1,"PUERTO__MEMORIA");
+
+    return;
+
+   }
+
+
+void terminar_programa(){
+    if(logger != NULL){
+		log_destroy(logger);
+	}
+	if(config1 != NULL){
+		config_destroy(config1);
+	}
+    return;
 }
