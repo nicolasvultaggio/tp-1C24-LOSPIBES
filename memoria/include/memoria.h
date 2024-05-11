979ef2d8c8b21b5fb2fcdefd16949ad7c8439a36
Marcelo Cabezas Flores <143379325+Marcelocabezasflores@users.noreply.github.com>
2024-05-11 18:45:44 +0000

    Update memoria.h
    rama memoria/memoria.h a MAIN

--- memoria/include/memoria.h
+++ memoria/include/memoria.h
@@ -19,4 +19,52 @@ int fd_conexion_io;
 
 void terminar_programa();
 void leer_configuraciones();
+//2do checkpoint
+int puerto_escucha;
+int tam_memoria;
+int tam_pagina;
+char* path_instrucciones;
+int retardo_respuesta;
+*************
+char* memoria;
+typedef enum{
+    SET,
+    MOV_IN,
+    MOV_OUT,
+    SUM,
+    SUB,
+    JNZ,
+    RESIZE,
+    COPY_STRING,
+    WAIT,
+    SIGNAL,
+    IO_STDIN_READ,
+    IO_STDIN_READ,
+    IO_STDOUT_WRITE,
+    IO_FS_CREATE,
+    IO_FS_DELETE,
+    IO_FS_TRUNCATE,
+    IO_FS_WRITE,
+    IO_FS_READ,
+    EXIT,
+
+}codigo_instrucciones;
+
+typedef struct {
+    codigo_instrucciones instruccion1;
+    char* parametro1;
+    char* parametro2;
+    char* parametro3;
+    char* parametro4;
+    char* parametro5;
+
+}t_instruccion;
+typedef struct{
+    char* path;
+    int pid;
+}t_datos_proceso;
+typedef struct {
+	int pid;
+	t_list* instrucciones;
+} t_proceso_instrucciones;
 #endif
