af1baa4b82a8b419fb3a7cae7d003c92f30f1e78
nicolasvultaggio <131707089+nicolasvultaggio@users.noreply.github.com>
2024-05-11 15:26:34 -0300

    Update entradasalida.h

--- entradasalida/include/entradasalida.h
+++ entradasalida/include/entradasalida.h
@@ -16,14 +16,36 @@ char* ip_kernel;
 char* puerto_kernel;
 char* ip_memoria;
 char* puerto_memoria;
+int tiempo_unidad_trabajo;
 char* mensaje_ok_memoria;
 char* mensaje_ok_kernel;
 
+char * nombre_de_interfaz;
+char * path_configuracion;
+char * tipo_de_interfaz;
+int type_interfaz;
+
+t_log* logger_obligatorio;
 t_log* logger_io;
 t_config* config_io;
 
 void leer_configuraciones();
 void terminar_programa();
 bool iniciar_conexiones();
+
+void atender_kernel();
+void informar_nombre();
+void atender_instruccion();
+char * recibir_instruccion_de_kernel();
+
+void atender_GENERICA();
+void atender_STDIN();
+void atender_STDOUT();
+void atender_DIALFS();
+
+void avisar_operacion_realizada_kernel();
+
+
+//mover a protocolo asi kernel lo conoce tambien
 
 #endif
