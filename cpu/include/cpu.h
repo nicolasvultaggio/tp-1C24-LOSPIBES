59d2162170e0a58a8089ca642e632ba86331ca8d
nicolasvultaggio <pvultaggio@frba.utn.edu.ar>
2024-07-09 22:29:52 +0000

    algo me quedo en el .h

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -122,7 +122,6 @@ bool es_entrada_TLB_de_PID(void * un_nodo_tlb );
 size_t size_registro(enum_reg_cpu registro);
 enum_reg_cpu registro_to_enum(char * registro);
 void * capturar_registro(enum_reg_cpu registro);
-uint32_t recibir_lectura_memoria();
 t_list * obtener_traducciones(uint32_t direccion_logica_i, uint32_t tamanio_a_leer );
 
 int solicitar_frame_memory(int numero_pagina);
